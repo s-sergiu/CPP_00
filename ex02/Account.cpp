a18e872d3dbb2ac20c2fab1dec14b9031818060d
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-06-30 06:50:20 +0200

    update

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,28 +1,38 @@
 
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
 
 Account::~Account() 
 {
 	std::cout<<"destructor"<<std::endl;
 }
 
+int Account::_nbAccounts;
+int Account::_totalNbDeposits;
+int Account::_totalNbWithdrawals;
+int Account::_totalAmount;
+
 Account::Account(int initial_deposit) 
 {
-	(void)initial_deposit;
-	std::cout<<"constructor"<<std::endl;
+	static int	counter;
+	Account::_nbDeposits = initial_deposit;
+	Account::_totalAmount += initial_deposit;
+	Account::_nbAccounts++;
+	std::cout<<"[19920104_091532] "<<"index:"<<counter++<<";";
+	std::cout<<"amount:"<<initial_deposit<<";";
+	std::cout<<"created"<<std::endl;
 }
 
 void Account::makeDeposit(int deposit)
 {
-	(void)deposit;
-	std::cout<<"make deposit"<<std::endl;
+	std::cout<<"make deposit"<<deposit<<std::endl;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
 {
-	(void)withdrawal;
-	std::cout<<"make withdrawal"<<std::endl;
+	Account::_nbWithdrawals = withdrawal;
+	std::cout<<"make withdrawal"<<withdrawal<<std::endl;
 	return (true);
 }
 
@@ -39,5 +49,11 @@ void Account::displayStatus(void) const
 
 void Account::displayAccountsInfos(void) 
 {
-	std::cout<<"display accinfo"<<std::endl;
+	std::cout<<"[19920104_091532] ";
+	std::cout<<"accounts:"<<_nbAccounts<<";";
+	std::cout<<"total:"<<_totalAmount<<";";
+	std::cout<<"deposits:"<<_totalNbDeposits<<";";
+	std::cout<<"withdrawals:"<<_totalNbWithdrawals;
+	std::cout<<std::endl;
 }
+
